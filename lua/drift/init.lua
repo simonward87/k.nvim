a524b3113f37d4c1f4376b2f81f7e5aa30da991a
Simon Ward <39803787+simonward87@users.noreply.github.com>
2024-03-17 11:52:28 +0000

    Refactor

--- lua/drift/init.lua
+++ lua/drift/init.lua
@@ -7,6 +7,20 @@ function M.setup(opts)
 	end
 end
 
+local function initializeVars(theme_style)
+	if theme_style == nil or theme_style == "" then
+		theme_style = "dark" -- set default
+	end
+
+	if string.len(theme_style) > 5 then
+		vim.o.background = string.sub(theme_style, 1, -3)
+	end
+
+	vim.g.colors_name = "drift-" .. theme_style
+
+	return "drift.palette-" .. theme_style
+end
+
 function M.load(theme_style)
 	local settings = require("drift.settings")
 	local opts = settings.opts
@@ -18,31 +32,17 @@ function M.load(theme_style)
 
 	vim.o.termguicolors = true
 
-	local palette_filename = "drift.palette-dark"
-
-	if theme_style == "light" or theme_style == "lighter" then
-		vim.o.background = "light"
-		vim.g.colors_name = "drift-" .. theme_style
-		palette_filename = "drift.palette-" .. theme_style
-	elseif theme_style == "dark" or theme_style == "darker" then
-		vim.o.background = "dark"
-		vim.g.colors_name = "drift-" .. theme_style
-		palette_filename = "drift.palette-" .. theme_style
-	else
-		vim.o.background = "dark"
-		vim.g.colors_name = "drift-dark"
-	end
-
+	local palette_file = initializeVars(theme_style)
 	local util = require("drift.util")
 	local groups = require("drift.groups")
-	local palette = require(palette_filename)
+	local palette = require(palette_file)
 
 	for _, group in ipairs(groups) do
 		group = group.highlight(palette, opts)
 		util.initialize(group)
 	end
 
-	-- removes semantic highlighting until implemented properly
+	-- Remove semantic highlighting until implemented
 	for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
 		vim.api.nvim_set_hl(0, group, {})
 	end
