415ae89010b204bdfc795bf9c9bc309dc895adaf
Simon Ward <39803787+simonward87@users.noreply.github.com>
2024-03-19 11:21:19 +0000

    Refactor init

--- lua/drift/init.lua
+++ lua/drift/init.lua
@@ -7,38 +7,34 @@ function M.setup(opts)
 	end
 end
 
-local function initializeVars(theme_style)
-	if theme_style == nil or theme_style == "" then
-		theme_style = "dark" -- set default
+-- style: "light" or "lighter" or "dark" or "darker"
+function M.load(style)
+	if style == nil or style == "" then
+		style = "dark"
 	end
-
-	if string.len(theme_style) > 5 then
-		vim.o.background = string.sub(theme_style, 1, -3)
-	end
-
-	vim.g.colors_name = "drift-" .. theme_style
-
-	return "drift.palette-" .. theme_style
-end
-
-function M.load(theme_style)
-	local settings = require("drift.settings")
-	local opts = settings.opts
 
 	vim.api.nvim_command("hi clear")
 	if vim.fn.exists("syntax_on") then
 		vim.api.nvim_command("syntax reset")
 	end
 
-	vim.o.termguicolors = true
+	local background = style
 
-	local palette_file = initializeVars(theme_style)
-	local util = require("drift.util")
+	if string.len(background) > 5 then
+		background = string.sub(background, 1, -3)
+	end
+
+	vim.o.background = background
+	vim.o.termguicolors = true
+	vim.g.colors_name = "drift-" .. style
+
 	local groups = require("drift.groups")
-	local palette = require(palette_file)
+	local palette = require("drift.palette-" .. style)
+	local settings = require("drift.settings")
+	local util = require("drift.util")
 
 	for _, group in ipairs(groups) do
-		group = group.highlight(palette, opts)
+		group = group.highlight(palette, settings.opts)
 		util.initialize(group)
 	end
 
